52f8bf66312875c9f2db82242fe8c17d9f5b03bb
CertTester <mltsttest@gmail.com>
2025-04-17 03:28:58 +0530

    playgorund challenge1 completed

--- Main.java
+++ Main.java
@@ -3,7 +3,8 @@ public class Main {
         // Test Case 1: Stock Analysis
         StockPortfolio stock = new StockPortfolio("AAPL", 10, 150.0);
         stock.updatePrice(160.0);
-        System.out.println(stock.calculateProfit());
-        System.out.println(stock.getCurrentValue());
+        System.out.println("Profit/Loss: $" + stock.calculateProfit());
+        System.out.println("Current Value: $" + stock.getCurrentValue());
+        System.out.println("Current Value: $" + stock.getCurrentValue());
     }
 }
